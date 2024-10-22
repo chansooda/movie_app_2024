07f8f2f1a2dbe8c0adc134a7540687d5726a3877
chansooda <munchamsu3263@naver.com>
2024-10-22 20:22:15 +0900

    Test:영화 api 를 이용해서 관련 목록 조회하기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState ,useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App =() => {
+  const [isLoading,setIsLoading] =useState(true);
+  const [movies,setMovies] = useState([]);
 
+//비동기 식으로 처리 
+  const getMovies = async() =>{
+    try{
+      const apiKey ='2276106737c3cf3fbd48059560a3dd54';
+      const targetDate ='20241021';
+
+      const response = await axios.get(`https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${apiKey}&targetDt=${targetDate}`);
+      console.log(response.data);
+      const movieData =response.data.boxOfficeResult.dailyBoxOfficeList;
+
+      setMovies(movieData);
+      setIsLoading(false);
+    }catch(error){
+      console.error("Error fetching movies:",error);
+      setIsLoading(false);
+    }
+  };
+  useEffect(()=>{
+    getMovies();
+  },[]);
+
+  return(
+    <div>
+      {isLoading ?(
+        "Loding..."
+      ) : (
+        <ul>
+          {movies.map((movie, index) =>(
+            <li key={index}>
+            {movie.image && (
+              <img src={movie.image} alt={movie.movieNm} />
+          )}
+            <h1>{movie.movieNm}</h1>
+            <p>개봉일:{movie.openDt}</p>
+            <p>박스오피스 :{movie.rank}</p> 
+            <p>해당일 관객수:{movie.audiCnt}</p>
+            <p>해당일 매출액:{movie.salesAmt}원</p> 
+            <p>총 누적 관객수:{movie.audiAcc}명</p>
+          </li>
+          ))}
+        </ul>
+        )}
+        </div>
+      );
+    };
 export default App;
